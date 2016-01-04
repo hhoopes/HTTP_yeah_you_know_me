dae037e215136b7ab6335485e3acceeac5f12417
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-01-04 16:44:07 -0700

    created gemfile

--- lib/iteration_1.rb
+++ lib/iteration_1.rb
@@ -1,35 +1,62 @@
 require 'socket'
-require_relative 'Iter_1'
+require_relative 'output_diagnostic'
 
-iter_1 = Iter_1.new
-tcp_server = TCPServer.new(9292)
-loop do
-client = tcp_server.accept
+class Iteration_1
+  attr_reader :client
+  attr_accessor :request_lines
 
-puts "Ready for a request"
-request_lines = []
-while line = client.gets and !line.chomp.empty?
-  request_lines << line.chomp
+  def initialize
+    @request_lines = []
+    @output = ""
+    @headers = []
+    @client = TCPServer.new(9292)
+  end
+
+  def ready_for_request
+    puts "Ready for a request"
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+
+    end
+  end
+
+  def get_request
+    puts "Got this request:"
+    puts request_lines.inspect
+  end
+
+  def send_response
+    puts "Sending response."
+    response = "<pre>" + request_lines.join("\n") + "</pre>"
+    @output = "<html><head></head><body>#{response}</body></html>"
+    @headers = ["http/1.1 200 ok",
+              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    client.puts @headers
+    client.puts @output
+  end
+
+  def write_response
+    puts ["Wrote this response:", @headers, @output].join("\n")
+  end
+
+  def close_client
+    client.close
+    puts "\nResponse complete, exiting."
+  end
 
 end
 
-iter_1.increment
+  iter_1 = Iteration_1.new
 
-puts "Got this request:"
-puts request_lines.inspect
+  iter_1.ready_for_request
 
-puts "Sending response."
-response = "<pre>" + iter_1.hello + "</pre>"
-output = "<html><head></head><body>#{response}</body></html>"
-headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-client.puts headers
-client.puts output
+  iter_1.get_request
 
-puts ["Wrote this response:", headers, output].join("\n")
-client.close
-puts "\nResponse complete, exiting."
-end
+  iter_1.send_response
+
+  iter_1.write_response
+
+  iter_1.close_client
