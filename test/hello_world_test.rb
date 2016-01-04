ed027716faa4150d238f7d7303db3498b1d16461
hhoopes <heidih@gmail.com>
2016-01-04 15:14:02 -0700

    Add Hello World test file

---
+++ test/hello_world_test.rb
@@ -0,0 +1,14 @@
+require 'minitest'
+require "hurley"
+require_relative 'test_helper'
+require_relative 'hello_world'
+
+class HelloWorldTest < Minitest::Test
+  def setup
+
+  end
+
+  def test_server_is
+
+
+end
