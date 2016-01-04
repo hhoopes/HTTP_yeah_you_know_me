5272a217c709598e8717de5f005d2997cc0aac38
hhoopes <heidih@gmail.com>
2016-01-04 13:24:31 -0700

    add test files for playing with iter 0

---
+++ test/Iter_1.rb
@@ -0,0 +1,14 @@
+class Iter_1
+
+  def initialize
+    @iteration_number = 0
+  end
+
+  def hello
+    "Hello, World!(#{@iteration_number})"
+  end
+
+  def increment
+    @iteration_number += 1
+  end
+end
---
+++ test/Iterat_1.rb
@@ -0,0 +1,7 @@
+class Iter_1
+
+  def initialize
+    @iteration_number = 0
+  end
+
+end
