952fe832b9ab969c67ae4fc9b295b56738e8174e
hhoopes <heidih@gmail.com>
2016-01-07 14:32:59 -0700

    Edit variables

--- lib/server.rb
+++ lib/server.rb
@@ -4,13 +4,11 @@ require 'socket'
 require 'request'
 
 class Server
-  attr_reader  :tcpserver, :request_handler
-  attr_accessor  :client, :request_lines, :output, :headers, :shutdown
+  attr_reader  :tcpserver, :request_handler, :output, :headers, :shutdown, :request_lines
+  attr_accessor  :client
 
   def initialize
     @request_lines = []
-    @output = ""
-    @headers = []
     @tcpserver = TCPServer.new(9292)
     @shutdown_flag = false
     @request_handler = Request.new
@@ -31,8 +29,8 @@ class Server
   end
 
   def process_request
-    response_text = @request_handler.process(request_lines)
-    @shutdown_flag = @request_handler.shutdown_flag
+    response_text = request_handler.process(request_lines)
+    @shutdown_flag = request_handler.shutdown_flag
     send_response(response_text)
   end
 
@@ -45,12 +43,12 @@ class Server
               "server: ruby",
               "content-type: text/html; charset=iso-8859-1",
               "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-    client.puts @headers
-    client.puts @output
+    client.puts headers
+    client.puts output
   end
 
   def write_response_to_terminal
-    puts ["Wrote this response:", @headers, @output].join("\n")
+    puts ["Wrote this response:", headers, output].join("\n")
   end
 
   def close_client
